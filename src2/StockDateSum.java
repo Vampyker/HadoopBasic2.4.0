a6ac0a589becd6412253d0c2548e430f4bfbcfed
Vampyker <starmento@gmail.com>
2015-05-28 01:27:00 -0400

    Initial Check-in Hadoop Project 2
    Uses the same data set

---
+++ src2/StockDateSum.java
@@ -0,0 +1,67 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+public class StockDateSum {
+
+  public static void main(String[] args) {
+
+	if(args.length < 2)
+	{
+		System.out.println("You must have at least two arguments.");
+		return;
+	}
+	
+	
+	System.out.println("arg1: " + args[0]);
+	System.out.println("arg2: " + args[1]);
+	  
+	JobClient client = new JobClient();
+    JobConf conf = new JobConf(StockDateSum.class);
+
+    // specify output types
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(DoubleWritable.class);
+
+    // specify input and output dirs
+    
+    // 1 FILE!
+    //FileInputFormat.addInputPath(conf, new Path("hdfs://localhost:9000/input3"));
+    
+    // ALL FILES!
+    //FileInputFormat.addInputPath(conf, new Path("hdfs://localhost:9000/input2/hadoop project data"));
+    //
+    FileInputFormat.addInputPath(conf, new Path(args[0].toString()));
+
+    //FileInputFormat.addInputPath(conf, new Path("hdfs://localhost:9000/input"));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1].toString()));
+    //FileOutputFormat.setOutputPath(conf, new Path("output"));
+
+    // specify a mapper
+    conf.setMapperClass(StockDateSumMapper.class);
+
+    // specify a reducer
+    conf.setReducerClass(StockDateSumReducer.class);
+    conf.setCombinerClass(StockDateSumReducer.class);
+
+    client.setConf(conf);
+    try {
+      System.out.println("Pre-RunJob - OK (StockDateSum)");
+      
+      System.out.println("Local Dirs: " + conf.getLocalDirs()[0].toString());
+      //System.out.println("Input Format: " + conf.getInputFormat().toString());
+      //System.out.println("Input Format: " + conf.getCredentials().toString());
+      
+      
+      JobClient.runJob(conf);
+      System.out.println("Post-RunJob - OK");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src2/StockDateSumReducer.java
@@ -0,0 +1,37 @@
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class StockDateSumReducer extends MapReduceBase
+    implements Reducer<Text, IntWritable, Text, DoubleWritable> {
+
+  public void reduce(Text key, Iterator values,
+      OutputCollector output, Reporter reporter) throws IOException {
+
+	try
+	{
+		double sum = 0;
+    	while (values.hasNext()) {
+    		DoubleWritable value = (DoubleWritable) values.next();
+      		sum += value.get(); // process value
+    	}
+
+    	output.collect(key, new DoubleWritable(sum));
+	}
+	catch(Exception ex)
+	{
+		System.out.println("Error found:" + "1");
+		System.out.println("Error found:" + ex.getStackTrace()[0]);
+	}
+  }
+}
